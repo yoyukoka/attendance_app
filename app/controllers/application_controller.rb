8cbf62d7814a20b257c6c386ce601e8d3288bd0f
yoyukoka <167397586+yoyukoka@users.noreply.github.com>
2025-03-31 14:15:09 +0900

    css当てている途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # ログイン後のリダイレクト先を設定
+  def after_sign_in_path_for(resource)
+    edit_user_registration_path # /user/edit へのパス
+  end
 end
